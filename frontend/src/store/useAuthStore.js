79c4e5316ab60ef70e35a3701924e8a1e857fd0a
Saad Rimeh <saad.rimeh.2001.01@gmail.com>
2025-11-11 13:59:18 +0300

    frontend codebase setup

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,11 @@
+import{create}from 'zustand';
+
+export const useAuthStore = create((set)=>({
+    authUser:{name:"saad",_id:"12345",age:25},
+isLoggedIn:false,
+isLoading:false,
+    login:()=>{
+        console.log("we just logged in");
+        set({isLoggedIn:true  , isLoading:true});
+    },
+}))
